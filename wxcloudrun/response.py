cd50db1e46fe27a8085b334649ba405e27f49606
Lingfengyu <1378990254@qq.com>
2024-02-01 20:24:23 +0800

    Update response.py
    指定返回状态码

--- wxcloudrun/response.py
+++ wxcloudrun/response.py
@@ -5,13 +5,11 @@ from flask import Response
 
 def make_succ_empty_response():
     data = json.dumps({'code': 0, 'data': {}})
-    return Response(data, mimetype='application/json')
-
+    return Response(data, status=200, mimetype='application/json')
 
 def make_succ_response(data):
     data = json.dumps({'code': 0, 'data': data})
-    return Response(data, mimetype='application/json')
-
+    return Response(data, status=200, mimetype='application/json')
 
 def make_err_response(err_msg):
     data = json.dumps({'code': -1, 'errorMsg': err_msg})
@@ -20,9 +18,9 @@ def make_err_response(err_msg):
 def score_char_response(char, score):
     res = {'code': 0, 'char': char, 'score': score}
     data = json.dumps(res)
-    return Response(data, mimetype='application/json')
+    return Response(data, status=200, mimetype='application/json')
 
 def score_time_response(char, score, time):
     res = {'code': 0, 'char': char, 'score': score, 'time': time}
     data = json.dumps(res)
-    return Response(data, mimetype='application/json')
+    return Response(data, status=200, mimetype='application/json')
