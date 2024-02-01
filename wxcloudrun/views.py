095db8837372d077d89b3a8acaf6d38903d098c5
Lingfengyu <1378990254@qq.com>
2024-02-01 20:04:00 +0800

    移除debug模式
    去除原有的页面

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -29,10 +29,12 @@ def init():
     try:
         f = open('./model/model.pt', 'r')
         f.close()
+        print("model load true")
     except FileNotFoundError:
         for i in range(0, 3):
             try:
                 client.download_file(Bucket=bucket, Key=model_path, DestFilePath=local_path)
+                print("model download true")
                 break
             except CosClientError or CosServiceError as e:
                 print(e)
@@ -110,62 +112,59 @@ def queryScore():
         else:
             return score_time_response(scoreitem.char, scoreitem.score, scoreitem.time)
 
+# @app.route('/')
+# def index():
+#     """
+#     :return: 返回index页面
+#     """
+#     return render_template('index.html')
 
-@app.route('/')
-def index():
-    """
-    :return: 返回index页面
-    """
-    return render_template('index.html')
+# @app.route('/api/count', methods=['POST'])
+# def count():
+#     """
+#     :return:计数结果/清除结果
+#     """
 
+#     # 获取请求体参数
+#     params = request.get_json()
 
-@app.route('/api/count', methods=['POST'])
-def count():
-    """
-    :return:计数结果/清除结果
-    """
+#     # 检查action参数
+#     if 'action' not in params:
+#         return make_err_response('缺少action参数')
 
-    # 获取请求体参数
-    params = request.get_json()
+#     # 按照不同的action的值，进行不同的操作
+#     action = params['action']
 
-    # 检查action参数
-    if 'action' not in params:
-        return make_err_response('缺少action参数')
+#     # 执行自增操作
+#     if action == 'inc':
+#         counter = query_counterbyid(1)
+#         if counter is None:
+#             counter = Counters()
+#             counter.id = 1
+#             counter.count = 1
+#             counter.created_at = datetime.now()
+#             counter.updated_at = datetime.now()
+#             insert_counter(counter)
+#         else:
+#             counter.id = 1
+#             counter.count += 1
+#             counter.updated_at = datetime.now()
+#             update_counterbyid(counter)
+#         return make_succ_response(counter.count)
 
-    # 按照不同的action的值，进行不同的操作
-    action = params['action']
+#     # 执行清0操作
+#     elif action == 'clear':
+#         delete_counterbyid(1)
+#         return make_succ_empty_response()
 
-    # 执行自增操作
-    if action == 'inc':
-        counter = query_counterbyid(1)
-        if counter is None:
-            counter = Counters()
-            counter.id = 1
-            counter.count = 1
-            counter.created_at = datetime.now()
-            counter.updated_at = datetime.now()
-            insert_counter(counter)
-        else:
-            counter.id = 1
-            counter.count += 1
-            counter.updated_at = datetime.now()
-            update_counterbyid(counter)
-        return make_succ_response(counter.count)
+#     # action参数错误
+#     else:
+#         return make_err_response('action参数错误')
 
-    # 执行清0操作
-    elif action == 'clear':
-        delete_counterbyid(1)
-        return make_succ_empty_response()
-
-    # action参数错误
-    else:
-        return make_err_response('action参数错误')
-
-
-@app.route('/api/count', methods=['GET'])
-def get_count():
-    """
-    :return: 计数的值
-    """
-    counter = Counters.query.filter(Counters.id == 1).first()
-    return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+# @app.route('/api/count', methods=['GET'])
+# def get_count():
+#     """
+#     :return: 计数的值
+#     """
+#     counter = Counters.query.filter(Counters.id == 1).first()
+#     return make_succ_response(0) if counter is None else make_succ_response(counter.count)
